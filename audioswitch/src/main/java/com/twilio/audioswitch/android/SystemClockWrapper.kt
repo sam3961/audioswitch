65d599739d564ad474a86cbd26379cd8f03a45c3
John Qualls <jaltonqualls@twilio.com>
2020-10-14 16:13:05 -0600

    Only keep docs dir on this branch [skip ci]

--- audioswitch/src/main/java/com/twilio/audioswitch/android/SystemClockWrapper.kt
+++
@@ -1,8 +0,0 @@
-package com.twilio.audioswitch.android
-
-import android.os.SystemClock
-
-internal class SystemClockWrapper {
-
-    fun elapsedRealtime() = SystemClock.elapsedRealtime()
-}
--- audioswitch/src/main/java/com/twilio/audioswitch/bluetooth/BluetoothHeadsetConnectionListener.kt
+++
@@ -1,6 +0,0 @@
-package com.twilio.audioswitch.bluetooth
-
-internal interface BluetoothHeadsetConnectionListener {
-    fun onBluetoothHeadsetStateChanged(headsetName: String? = null)
-    fun onBluetoothHeadsetActivationError()
-}
